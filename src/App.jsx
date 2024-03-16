136181a3f6e79aaa4f6ad3f4b2d9846b2ddabebd
damilare atunbi <damilareatunbi@gmail.com>
2024-03-16 09:30:46 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,11 @@
-import { useState } from "react";
-import SignatureCanvas from "react-signature-canvas";
-import toast from "react-hot-toast";
-
+import toast, { Toaster } from "react-hot-toast";
+import Signature from "./pages/Signature";
 function App() {
-  const [sign, setSign] = useState();
-  const [url, setUrl] = useState();
-  const [isDisplay, setIsDisplay] = useState(false);
-
-  const handleClear = () => {
-    if (!sign.isEmpty()) {
-      sign.clear();
-      toast.success("content successfully cleared");
-    }
-    return;
-  };
-  const handleGenerate = () => {
-    if (sign.isEmpty()) {
-      toast.error("please sign on the plain space and Upload");
-      return;
-    }
-    setUrl(sign.getTrimmedCanvas().toDataURL("image/png"));
-    setIsDisplay(true);
-  };
-  const handleUpload = () => {
-    toast.success("uploaded successfuly");
-    sign.clear();
-    setIsDisplay(false);
-  };
-
-  console.log("the sign", sign);
-  console.log("the url", url);
   return (
-    <div className="main-body">
-      <p>Upload Signature</p>
-      <div className="sig-container">
-        <SignatureCanvas canvasProps={{ width: 450, height: 200, className: "sigCanvas" }} ref={(data) => setSign(data)} />
-      </div>
-      <div className="sig-btn">
-        <button className="but-btn" onClick={handleClear}>
-          {" "}
-          Clear
-        </button>
-        <button className="but-btn" onClick={handleGenerate}>
-          Save
-        </button>
-      </div>
-
-      {isDisplay && url.length > 0 && (
-        <div className="display">
-          <img src={url} alt="sig" className="sig-image" />
-          <button className="but-btn" type="button" onClick={handleUpload}>
-            Upload
-          </button>
-        </div>
-      )}
-    </div>
+    <>
+      <Toaster position="top-right" />
+      <Signature />
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Toaster } from "react-hot-toast";
+// import toast, { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <Toaster position="top-right" />
+    {/* <Toaster position="top-right" /> */}
   </React.StrictMode>
 );
